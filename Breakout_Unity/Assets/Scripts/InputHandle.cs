0e8f8fff8a35c1d1d431572f9cc0d96f76110a6c
Andrej Petelin <petelin.andrej@amis.net>
2018-02-13 03:15:13 +0100

    Added InputHandle class

---
+++ Breakout_Unity/Assets/Scripts/InputHandle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+public static class InputHandle
+{
+	public static float Horizontal
+	{
+		get { return Input.GetAxis("horizontal"); }
+		private set { }
+	}
+
+
+	public static float Vertical
+	{
+		get { return Input.GetAxis("vertical"); }
+		private set { }
+	}
+
+
+	public static bool Enter
+	{
+		get { return Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter); }
+		private set { }
+	}
+
+
+    public static bool Pause
+    {
+        get { return Input.GetKey(KeyCode.Space); }
+        private set { }
+    }
+}
