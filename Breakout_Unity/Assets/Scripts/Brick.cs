87da2fd269cdabc8ffe8f045518deb8ed7d261b9
Irene Naya <irenayamackay@gmail.com>
2018-02-17 01:01:18 -0300

    Brick prefab and class added

--- Breakout_Unity/Assets/Scripts/Brick.cs
+++ Breakout_Unity/Assets/Scripts/Brick.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
+	public Sprite[] sprites;
+	SpriteRenderer renderer;
+	int index;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		renderer = GetComponent<SpriteRenderer> ();
+
+	}
+
+	public void SetTier (int tier) {
+		index = tier;
+		renderer.sprite = sprites [index];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (index > 0) {
+			renderer.sprite = sprites [--index];
+		}
+		else {
+			Destroy (gameObject);
+		}
 	}
 }
