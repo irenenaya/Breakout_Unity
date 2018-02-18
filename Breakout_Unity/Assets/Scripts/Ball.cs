f3fbc4cc7680e462eaa0f9dcefa0e743ea958f2f
Andrej Petelin <petelin.andrej@amis.net>
2018-02-18 14:24:23 +0100

    Added music, added SetVelocity to ball

--- Breakout_Unity/Assets/Scripts/Ball.cs
+++ Breakout_Unity/Assets/Scripts/Ball.cs
@@ -6,19 +6,27 @@ public class Ball : MonoBehaviour
 {
     public Sprite[] sprites;
 
+    new Rigidbody2D rigidbody;
     new CircleCollider2D collider;
     new SpriteRenderer renderer;
+
     // Use this for initialization
     void Awake()
     {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
         collider = gameObject.GetComponent<CircleCollider2D>();
         renderer = gameObject.GetComponent<SpriteRenderer>();
         renderer.sprite = sprites[Random.Range(0, sprites.Length)];
 	}
 	
+    void SetVelocity(Vector2 vel)
+    {
+        rigidbody.velocity = vel;
+    }
+
 	// Update is called once per frame
 	void Update()
     {
-		
+
 	}
 }
