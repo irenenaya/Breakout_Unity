3b3015f83b0e35b29ddced7ec7c5b857b383a859
Andrej Petelin <petelin.andrej@amis.net>
2018-02-19 01:54:00 +0100

    Added LevelLoadState to state machine, added screen boundaries, added PlayUI

--- Breakout_Unity/Assets/Scripts/LoadLevelStateBehaviour.cs
+++ Breakout_Unity/Assets/Scripts/LoadLevelStateBehaviour.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ServeStateBehaviour : GeneralStateBehaviour {
-
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//	controller.LoadNextScene (SceneConstants.PLAY);
-	//}
+public class LoadLevelStateBehaviour : GeneralStateBehaviour
+{
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller.LoadNextScene(SceneConstants.PLAY);
+    }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
