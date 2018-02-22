5928215732c3aedacf46ac6d48d23795acc46e2f
Andrej Petelin <petelin.andrej@amis.net>
2018-02-22 20:19:31 +0100

    Added fix to particles BrickHit prefab - Order in layer set to 1, added HighScoreHandler.cs to SceneController object

---
+++ Breakout_Unity/Assets/Scripts/BinaryHeap.cs
@@ -0,0 +1,133 @@
+﻿/**
+ * BinaryHeap.cs - a generic Binary Heap implementation in C#
+ * 
+ * T must implement IComparable
+ * 
+ * BinaryHeap<T> bh = new BinaryHeap<T>();  creates empty binary heap of Ts with natural order
+ * BinaryHeap<T> bh = new BinaryHeap<T>(compareFunc);  creates empty binary heap with a comparison function
+ * i.e. for reverse sorting, sorting by alternate criterion etc.
+ * 
+ * use bh.push(x) to add x to the binary heap
+ * use bh.pop() to retrieve the top element. Throws IndexOutOfRangeException if called on empty heap.
+ * use bh.peek() to retrieve but not remove the top element. Throws IndexOutOfRangeException if called on empty heap.
+ */
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace DataStructures
+{
+    public delegate int Compare(IComparable x, IComparable y);
+
+    public class BinaryHeap<T> where T:IComparable
+    {
+        Compare comp;   // comparison function, pass delegate to constructor to use
+        List<T> data = new List<T>();
+
+        public int Size { get { return data.Count; } private set { } }
+                
+
+        public BinaryHeap() { comp = (x, y) => { return x.CompareTo(y); }; }
+
+        public BinaryHeap(Compare compareTo) { comp = compareTo; }
+
+
+        public bool IsEmpty() { return data.Count == 0; }
+
+
+        /**
+         * add element to the heap, reordering to maintain consistency
+         */
+        public void Push(T x)
+        {
+            data.Add(x);
+            int index = Size - 1;
+
+            while (index > 0)
+            {
+                int parent = ParentIndex(index);
+
+                if (comp(data[index], data[parent]) < 0)
+                {
+                    SwapAt(index, parent);
+                    index = parent;
+                }
+                else break;
+            }
+        }
+
+
+        /**
+         * removes and returns the top element from the heap, reordering to maintain consistency
+         */
+        public T Pop()
+        {
+            if (IsEmpty()) throw new System.IndexOutOfRangeException("Called Pop() on empty BinaryHeap");
+
+            T ret = data[0];
+            int index = 0;
+
+            SwapAt(0, Size - 1);
+            data.RemoveAt(Size - 1);
+
+            while (index < Size)
+            {
+                int childIndex = MinChildIndex(index);
+
+                if (childIndex >= 0 && comp(data[childIndex], data[index]) < 0)
+                {
+                    SwapAt(index, childIndex);
+                    index = childIndex;
+                }
+                else break;
+            }
+
+            return ret;
+        }
+
+        /**
+         * get top element without removing it
+         */
+        public T Peek()
+        {
+            if (IsEmpty()) throw new System.IndexOutOfRangeException("Called Peek() on empty BinaryHeap");
+            return data[0];
+        }
+
+
+        /**
+         * -------- INTERNAL HELPER FUNCTIONS ---------
+         */
+         
+        int ParentIndex(int i)
+        {
+            return (i + 1) / 2 - 1;
+        }
+
+
+        /**
+         * finds the array index of the minimum of the two children of element at index i
+         */
+        int MinChildIndex(int i)
+        {
+            int childR = (i + 1) * 2;
+            int childL = childR - 1;
+
+            if (childL >= Size) return -1;
+            else if (childR < Size && comp(data[childR], data[childL]) < 0) return childR;
+            else return childL;
+        }
+
+        /**
+         * swaps elements at indices i and j in the array
+         */
+        void SwapAt(int i, int j)
+        {
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+}
---
+++ Breakout_Unity/Assets/Scripts/HighScoreHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataStructures;
+
+public class HighScoreHandler : MonoBehaviour
+{
+    BinaryHeap<ScoreboardEntry> data = new BinaryHeap<ScoreboardEntry>();
+
+	// Use this for initialization
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public class ScoreboardEntry : IComparable
+    {
+        public string name;
+        public int score;
+
+        public ScoreboardEntry(string n, int s)
+        {
+            name = n;
+            score = s;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException("ScoreboardEntry.CompareTo argument is null!");
+            ScoreboardEntry other = obj as ScoreboardEntry;
+            if (other == null)
+                throw new ArgumentException("ScoreboardEntry.CompareTo - argument not ScoreboardEntry!");
+            return score.CompareTo(other.score);
+        }
+    }
+}
