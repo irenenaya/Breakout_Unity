e3b062912a1d605652bd0fb207067b5f8b3cad2a
Irene Naya <irenayamackay@gmail.com>
2018-02-25 01:49:36 -0300

    Game Over Display added

---
+++ Breakout_Unity/Assets/Scripts/GameOverDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Text text = GetComponent<Text> ();
+		text.text = "Final Score: " + GameParameters.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Breakout_Unity/Assets/Scripts/GameOverStateBehaviour.cs
+++ Breakout_Unity/Assets/Scripts/GameOverStateBehaviour.cs
@@ -8,6 +8,8 @@ public class GameOverStateBehaviour : GeneralStateBehaviour {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		HighScoreHandler handler = controller.GetComponent<HighScoreHandler> ();
 		animator.SetBool ("Highscorer", handler.DisplayData.Length < 10 || handler.DisplayData [9].score < GameParameters.score);
+		controller.UnloadScene (SceneConstants.PLAY);
+		controller.activateObject ("GameOver", true);
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -15,10 +17,10 @@ public class GameOverStateBehaviour : GeneralStateBehaviour {
 	//
 	//}
 
-	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	 //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		controller.activateObject ("GameOver", false);		
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
