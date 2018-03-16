270b7db83c2f0e51725746fc11235cf080c99b27
Andrej Petelin <petelin.andrej@amis.net>
2018-03-16 20:05:21 +0100

    Added background music manager script

---
+++ Breakout_Unity/Assets/Scripts/MusicController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    AudioSource music;
+
+	void Awake()
+    {
+        music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (InputHandle.Pause) music.pitch *= 0.67f;
+	}
+}
