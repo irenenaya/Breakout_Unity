fb0ab219b1d0734bb0bd17abbc7fc018d08e9f07
Andrej Petelin <petelin.andrej@amis.net>
2018-03-17 23:26:31 +0100

    Added non-working music controller script

--- Breakout_Unity/Assets/Scripts/MusicController.cs
+++ Breakout_Unity/Assets/Scripts/MusicController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour {
     AudioSource music;
+    bool lerping = false;
 
 	void Awake()
     {
         music = GetComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update()
+
+    // Update is called once per frame
+    void Update()
     {
-        if (InputHandle.Pause) music.pitch *= 0.95f;
-	}
+        Debug.Log("Update");
+        // if (InputHandle.Pause) music.pitch *= 0.95f;
+        if (lerping || Input.GetKeyDown(KeyCode.Tab))
+        {
+            lerping = true;
+            //TransitionPitch(1.0f, 0.0f, 2f);
+            music.pitch = Mathf.Lerp(1f, 0f, Time.unscaledDeltaTime);
+
+        }
+    }
 }
